207e6f1a0d3108e506f33ac9db1793a7aa037465
Pavel Uvarov <pavel_uvarov@epam.com>
2020-05-19 09:12:28 +0300

    fix planets nav

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -11,14 +11,13 @@ import { VenusFetcher } from '../store/VenusPage/reducer';
 
 export const NAV_ITEMS = [
     { to: '/', title: 'Picture of the day', exact: true },
-    { to: '/earth', title: 'Earth', prefetch: EarthFetcher.prefetch },
-    { to: '/moon', title: 'Moon', prefetch: MoonFetcher.prefetch },
-    { to: '/uranus', title: 'Uranus', prefetch: UranusFetcher.prefetch },
-    { to: '/jupiter', title: 'Jupiter', prefetch: JupiterFetcher.prefetch },
-    { to: '/mars', title: 'Mars', prefetch: MarsFetcher.prefetch },
     { to: '/mercury', title: 'Mercury', prefetch: MercuryFetcher.prefetch },
-    { to: '/neptune', title: 'Neptune', prefetch: NeptuneFetcher.prefetch },
-    { to: '/saturn', title: 'Saturn', prefetch: SaturnFetcher.prefetch },
     { to: '/venus', title: 'Venus', prefetch: VenusFetcher.prefetch },
+    { to: '/earth', title: 'Earth', prefetch: EarthFetcher.prefetch },
+    { to: '/mars', title: 'Mars', prefetch: MarsFetcher.prefetch },
+    { to: '/jupiter', title: 'Jupiter', prefetch: JupiterFetcher.prefetch },
+    { to: '/saturn', title: 'Saturn', prefetch: SaturnFetcher.prefetch },
+    { to: '/uranus', title: 'Uranus', prefetch: UranusFetcher.prefetch },
+    { to: '/neptune', title: 'Neptune', prefetch: NeptuneFetcher.prefetch },
     { to: '/pluto', title: 'Pluto', prefetch: PlutoFetcher.prefetch },
 ];
